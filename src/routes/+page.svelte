780796744505d3bbca8a4ed2221243248c2e3b01
kimgh06 <kkh061101@naver.com>
2025-01-12 11:00:02 +0900

    add emotion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { createClient } from "@supabase/supabase-js";
+  import facePaths from "../facePaths.json";
   let canvas: HTMLCanvasElement;
   let context: CanvasRenderingContext2D;
   let count = 0;
@@ -9,8 +10,19 @@
   const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey);
 
-  const pathData =
-    "M0.999969 99C5 8.49999 154.5 -65.5 201 99C203 133.667 239 231 57 206";
+  const pathData = facePaths;
+
+  type PathData = {
+    d: Path2D;
+    stroke: string;
+    strokeWidth?: number;
+    strokeLinecap: CanvasLineCap;
+  };
+  type PathsObject = {
+    normal: PathData[];
+    drag: PathData[];
+  };
+  let facePath: PathsObject;
 
   // 초기 변수
   const initValue = [0, 0];
@@ -29,7 +41,6 @@
   const touchableRange = 80;
 
   const update = () => {
-    const facePath = new Path2D(pathData);
     [windowWidth, windowHeight] = [
       window.innerWidth - 16,
       window.innerHeight - 300,
@@ -50,18 +61,6 @@
     context.clearRect(0, 0, canvas.width ?? 0, canvas.height ?? 0);
     context.lineWidth = 2;
 
-    // 가로 세로 점선 그리기
-    context.beginPath();
-    context.moveTo(centerX, 0);
-    context.lineTo(centerX, windowHeight);
-    context.strokeStyle = "gray";
-    context.stroke();
-    context.beginPath();
-    context.moveTo(0, centerY);
-    context.lineTo(windowWidth, centerY);
-    context.strokeStyle = "gray";
-    context.stroke();
-
     // 거리 그리기
     context.beginPath();
     context.moveTo(originObjectX, originObjectY);
@@ -76,9 +75,12 @@
     context.strokeStyle = "black";
 
     // 얼굴을 살구색으로 칠하기
-    context.fillStyle = "peachpuff";
-    context.fill(facePath);
-    context.stroke(facePath);
+    for (const face of facePath[dragging ? "drag" : "normal"]) {
+      context.strokeStyle = face.stroke;
+      context.lineWidth = face.strokeWidth ?? 1;
+      context.lineCap = face.strokeLinecap;
+      context.stroke(face.d);
+    }
     context.restore();
 
     // 볼 그리기 (부드럽게 휘어지게)
@@ -87,7 +89,7 @@
 
     // 볼 제어점과 끝점 설정
     const [controlX1, controlY1] = [objectX - 100, objectY - 50];
-    const [controlX2, controlY2] = [objectX - 100, objectY + 120]; // 제어점 X2, Y2
+    const [controlX2, controlY2] = [objectX - 100, objectY + 80]; // 제어점 X2, Y2
 
     // 볼 곡선 그리기
     context.bezierCurveTo(
@@ -95,11 +97,11 @@
       controlY1,
       controlX2,
       controlY2,
-      centerX,
-      centerY + faceTranslateY,
+      centerX - 40,
+      centerY + faceTranslateY + 5,
     );
 
-    context.fillStyle = "peachpuff"; // 살구색
+    context.fillStyle = "#ffffff00"; //투명색으로 채우기
     context.fill(); // 내부 채우기
 
     // 볼 외곽선 그리기
@@ -109,7 +111,7 @@
     // 홍조 넣기
     context.beginPath();
     context.arc(objectX, objectY, touchableRange - 50, 0, 2 * Math.PI);
-    context.fillStyle = "white";
+    context.fillStyle = "pink";
     context.fill();
 
     time += 0.016; // 60fps
@@ -149,15 +151,24 @@
     }
   };
 
-  const drag = (event: MouseEvent) => {
+  const drag = (event: MouseEvent | TouchEvent) => {
     if (dragging) {
       const rect = canvas.getBoundingClientRect();
-      let [newObjectX, newObjectY] = [
-        event.clientX - rect.left - offsetX,
-        event.clientY - rect.top - offsetY,
-      ];
+      let newObjectX, newObjectY;
+      if (event instanceof TouchEvent) {
+        const touch = event.touches[0];
+        [newObjectX, newObjectY] = [
+          touch.clientX - rect.left - offsetX,
+          touch.clientY - rect.top - offsetY,
+        ];
+      } else {
+        [newObjectX, newObjectY] = [
+          event.clientX - rect.left - offsetX,
+          event.clientY - rect.top - offsetY,
+        ];
+      }
       if (newObjectX > centerX) newObjectX = centerX;
-      if (newObjectY < centerY - 100) newObjectY = centerY - 100;
+      if (newObjectY < centerY) newObjectY = centerY;
       [objectX, objectY] = [newObjectX, newObjectY];
     }
   };
@@ -172,6 +183,25 @@
 
   onMount(() => {
     getCountFromSupabase();
+    facePath = { normal: [], drag: [] };
+    for (const path of pathData.normal) {
+      const face = {
+        d: new Path2D(path.d),
+        stroke: path.stroke,
+        strokeWidth: path.strokeWidth,
+        strokeLinecap: path.strokeLinecap as CanvasLineCap,
+      };
+      facePath.normal.push(face);
+    }
+    for (const path of pathData.drag) {
+      const face = {
+        d: new Path2D(path.d),
+        stroke: path.stroke,
+        strokeWidth: path.strokeWidth,
+        strokeLinecap: path.strokeLinecap as CanvasLineCap,
+      };
+      facePath.drag.push(face);
+    }
     context = canvas.getContext("2d")!;
     update();
   });
@@ -194,14 +224,15 @@
   on:pointermove={drag}
   on:mouseup={endDrag}
   on:mouseleave={endDrag}
+  on:touchmove={drag}
   on:touchend={endDrag}
 ></canvas>
 
 <div class="controls">
   <h3>총 당긴 횟수: {count}</h3>
   <span
-    >Object Position from Center: ({Math.floor(centerX - objectX)}px, {Math.floor(
-      centerY - objectY,
+    >Object Position from Center: ({Math.round(objectX - centerX)}px, {Math.round(
+      objectY - objectY,
     )}px)</span
   ><br />
 
